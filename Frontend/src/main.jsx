a56d41b8c01972ce770bba4f3ef8f046b08a3fc6
Aditya Shee <161288779+AdityaShee12@users.noreply.github.com>
2025-10-16 22:36:17 +0530

    Correct routing of this web app

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,20 +1,15 @@
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./App.css";
 import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "./app/store.js";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-]);
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <RouterProvider router={router} />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>
 );
